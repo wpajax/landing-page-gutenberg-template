b6f8701615fc08d5de83554db2a2f5325e190382
Ronald Huereca <rhuereca@updraftplus.com>
2020-10-26 15:50:43 -0500

    Re-configuring Webpack to have sourcemaps.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
+const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const BabelMinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = [
 	{
@@ -9,6 +9,7 @@ module.exports = [
 		},
 		output: {
 			filename: "[name].js",
+			sourceMapFilename: "[name].js.map",
 		},
 		module: {
 			rules: [
@@ -17,17 +18,7 @@ module.exports = [
 					exclude: /(node_modules|bower_components)/,
 					loader: "babel-loader",
 					options: {
-						presets: [
-							[
-								"@babel/preset-env",
-								{
-									targets: {
-										esmodules: true,
-									},
-								},
-							],
-							"@babel/preset-react",
-						],
+						presets: ["@babel/preset-env", "@babel/preset-react"],
 						plugins: [
 							"@babel/plugin-proposal-class-properties",
 							"@babel/plugin-transform-arrow-functions",
@@ -40,12 +31,35 @@ module.exports = [
 			// Use external version of React
 			react: "React",
 		},
+		devtool: "source-map",
+		optimization: {
+			minimize: true,
+			minimizer: [
+				new TerserPlugin({
+					terserOptions: {
+						ecma: undefined,
+						parse: {},
+						compress: true,
+						mangle: false,
+						module: false,
+						output: null,
+						toplevel: false,
+						nameCache: null,
+						ie8: false,
+						keep_classnames: undefined,
+						keep_fnames: false,
+						safari10: false,
+					},
+				}),
+			],
+		},
 	},
 	{
 		mode: process.env.NODE_ENV,
 		entry: {
 			style: ["./src/scss/style.scss"],
 		},
+		output: {},
 		module: {
 			rules: [
 				{
@@ -68,7 +82,7 @@ module.exports = [
 		},
 		plugins: [
 			new MiniCssExtractPlugin({
-				filename: "[name].css",
+				filename: "template.css",
 			}),
 		],
 	},
